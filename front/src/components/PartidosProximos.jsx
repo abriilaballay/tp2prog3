4ea121b5e9f74623943e46204754365e00e8be93
Mgomez65 <matigomez257@gmail.com>
2024-10-25 11:23:20 -0300

    arregle los proximos partidos

--- front/src/components/PartidosProximos.jsx
+++ front/src/components/PartidosProximos.jsx
@@ -1,48 +1,55 @@
-import { useEffect, useState } from "react";
+import React from 'react';
 
 const Proximo = () => {
-    const [datos, setDatos] = useState();
-    useEffect(() => {
-        const fetchPartidos = async () => {
-            const url = `https://v3.football.api-sports.io/fixtures?team=26&next=2 `;
-            try {
-                const response = await fetch(url, {
-                    method: 'GET',
-                    headers: {
-                        'x-apisports-key': "3c0692c78c39074a78a1a5d8cbe0d0a3",
-                        'x-rapidapi-host': "v3.football.api-sports.io"
-                    }
-                });
-                const respuesta = await response.json();
-                console.log(respuesta)
-                setDatos(respuesta.response);
-            } catch (error) {
-                console.error("Error fetching data:", error);
+    const datos = [
+        {
+            "teams": {
+                "home": {
+                    "name": "Argentina",
+                    "logo": "https://ssl.gstatic.com/onebox/media/sports/logos/1xBWyjjkA6vEWopPK3lIPA_96x96.png"
+                },
+                "away": {
+                    "name": "Paraguay",
+                    "logo": "https://ssl.gstatic.com/onebox/media/sports/logos/-FN-y84Al3dbth0hW1t5Qg_96x96.png"
+                }
             }
-        };
-        fetchPartidos();
-    }, []);
+        },
+        {
+            "teams": {
+                "home": {
+                    "name": "Argentina",
+                    "logo": "https://ssl.gstatic.com/onebox/media/sports/logos/1xBWyjjkA6vEWopPK3lIPA_96x96.png"
+                },
+                "away": {
+                    "name": "Perú",
+                    "logo": "https://ssl.gstatic.com/onebox/media/sports/logos/1ZizIpPB_eo-u8zYYjnFcg_96x96.png"
+                }
+            }
+        }
+    ];
+
     return (
-        <div  className="proximoPartidos">
-        <h2 className="tituloPartidos">Próximo partido de Argentina</h2>
-        {datos && datos.map((partido, index) => (
-            <div key={index} className="botonPartido">
-                <div className="column local">
-                    <div className="teamProximo">
-                        <img src={partido.teams.home.logo} alt={`${partido.teams.home.name} logo`} className="team-logo" />
-                        <span className="team-name">{partido.teams.home.name}</span>
+        <div className="proximoPartidos">
+            <h2 className="tituloPartidos">Próximo partido de Argentina</h2>
+            {datos && datos.map((partido, index) => (
+                <div key={index} className="botonPartido">
+                    <div className="column local">
+                        <div className="teamProximo">
+                            <img src={partido.teams.home.logo} alt={`${partido.teams.home.name} logo`} className="team-logo" />
+                            <span className="team-name">{partido.teams.home.name}</span>
+                        </div>
+                    </div>
+                    <div className="column vs">VS</div>
+                    <div className="column visitante">
+                        <div className="teamProximo">
+                            <img src={partido.teams.away.logo} alt={`${partido.teams.away.name} logo`} className="team-logo" />
+                            <span className="team-name">{partido.teams.away.name}</span>
+                        </div>
                     </div>
                 </div>
-                <div className="column vs">VS</div>
-                <div className="column visitante">
-                    <div className="teamProximo">
-                        <img src={partido.teams.away.logo} alt={`${partido.teams.away.name} logo`} className="team-logo" />
-                        <span className="team-name">{partido.teams.away.name}</span>
-                    </div>
-                </div>
-            </div>
-        ))}
-    </div>
-    )
-}
+            ))}
+        </div>
+    );
+};
+
 export default Proximo;
